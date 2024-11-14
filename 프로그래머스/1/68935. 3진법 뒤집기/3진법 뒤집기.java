9897b955bdc062022ab8bd5c3c25d52be0c4d7e1
star-pooh <eunki92@gmail.com>
2024-11-14 10:12:20 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -2,18 +2,11 @@ class Solution {
     public int solution(int n) {
         StringBuilder answer = new StringBuilder();
         
-        while (n != 0) {
+        while (n > 0) {
             answer.append(n % 3);
             n /= 3;
         }
         
-        int sum = 0;
-        String divNum = answer.toString();
-        int len = divNum.length();
-        for(int i = 0; i < len; i++) {
-            sum += (int) (Math.pow(3, len - i - 1) * (divNum.charAt(i) - '0'));
-        }
-        
-        return sum;
+        return Integer.parseInt(answer.toString(), 3);
     }
 }
