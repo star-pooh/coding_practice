fe8b94dd00edeeca3b5de9722e28c580b731537d
star-pooh <eunki92@gmail.com>
2024-11-25 10:16:51 +0900

    [level 1] Title: 소수 만들기, Time: 2.59 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if(primeCheck(sum)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+    
+    
+    private static boolean primeCheck(int num) {
+        int sqrt = (int) Math.sqrt(num);
+        
+        for (int i = 2; i <= sqrt; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
