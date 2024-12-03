9fd78d5d67db733353ab6b049860dc47e19fb06e
star-pooh <eunki92@gmail.com>
2024-12-03 10:52:08 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.35 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,22 +1,13 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
-        String[][] grid = new String[wallpaper.length][wallpaper[0].length()];
-        
-        for (int i = 0; i < wallpaper.length; i++) {
-            String row = wallpaper[i];
-            for (int j = 0; j < wallpaper[0].length(); j++) {
-                grid[i][j] = String.valueOf(row.charAt(j));
-            }
-        }
-        
         int lux = Integer.MAX_VALUE;
         int luy = Integer.MAX_VALUE;
         int rdx = Integer.MIN_VALUE;
         int rdy = Integer.MIN_VALUE;
         
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j].equals("#")) {
+        for (int i = 0; i < wallpaper.length; i++) {
+            for (int j = 0; j < wallpaper[i].length(); j++) {
+                if (wallpaper[i].charAt(j) == '#') {
                     lux = Math.min(lux, i);
                     luy = Math.min(luy, j);
                     rdx = Math.max(rdx, i);
