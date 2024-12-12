c1620dae990d74399ad61ee228b71f13ae63d0e7
star-pooh <eunki92@gmail.com>
2024-12-12 09:46:01 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -5,7 +5,7 @@ class Solution {
         memo[1] = 1;
         
         for (int i = 2; i <= n; i++) {
-            memo[i] = (memo[i - 2] % 1234567 + memo[i - 1] % 1234567) % 1234567;
+            memo[i] = (memo[i - 2] + memo[i - 1]) % 1234567;
         }
         
         return memo[n];
