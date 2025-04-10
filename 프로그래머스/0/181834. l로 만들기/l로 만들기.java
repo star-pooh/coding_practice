dd16f5d71cafaa69b11c5999a51a9414ae39dad8
star-pooh <eunki92@gmail.com>
2025-04-10 18:07:54 +0900

    [level 0] Title: l로 만들기, Time: 39.85 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,13 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        for (int i = 0; i < myString.length(); i++) {
-            char target = myString.charAt(i);
-            
-            if (target < 'l') {
-                myString = myString.replace(String.valueOf(target), "l");
-            }
-        }
-        
-        return myString;
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
