94272ffa7e96f9a3dd74d617491e5957ee9ced1e
star-pooh <eunki92@gmail.com>
2025-04-15 19:12:08 +0900

    [level 2] Title: 하노이의 탑, Time: 3.72 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,6 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+
 class Solution {
     public int[][] solution(int n) {
-        int[][] answer = {};
-        return answer;
+        List<int[]> process = new ArrayList<>();
+        hanoi(n, 1, 3, process);
+        
+        return process.toArray(new int[0][]);
+    }
+    
+    private void hanoi(int n, int from, int to, List<int[]> process) {
+        if (n == 1) {
+            process.add(new int[] {from, to});
+            return;
+        }
+
+        int empty = 6 - from - to;
+        
+        hanoi(n - 1, from, empty, process);
+        hanoi(1, from, to, process);
+        hanoi(n - 1, empty, to, process);
     }
 }
